0d2c8891749f15be46e11eabf2628eabb4341000
Mohamed A <mohmeda123@gmail.com>
2013-11-25 20:31:01 +0300

    Added FCMRM v0.1
    Added FCMRM v0.1. It still doesn't work as needed.

--- get_keywords_script.m
+++ get_keywords_script.m
@@ -1,4 +1,4 @@
-I4 = imread('../testing/108019.jpg');
-Q = grid_generate_keywords(I4, grid_kmeans_C,gPwJ_MLE,gPwJ_GRE  ,  gPbJ_MLE,gPbJ_GRE ,0.1, 0.7, 15, number_of_words);
-[tmp2 tmp2_IDX] = sort(Q,'descend');
+I4 = imread('../testing/231052.jpg');
+Q = grid_generate_keywords(I4, fCenter,fPwJ_MLE,fPwJ_GRE  ,  fPbJ_MLE,fPbJ_GRE ,0.1, 0.7, 15, number_of_words);
+[tmp2, tmp2_IDX] = sort(Q,'descend');
 words(tmp2_IDX(1:5))
