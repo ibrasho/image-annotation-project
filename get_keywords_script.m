c3f83c07634ff35f951bceeb4386627606560cfb
Mohamed A <mohmeda123@gmail.com>
2013-12-05 15:42:23 +0300

    Added comparison between FCMRM and CFCMRM

--- get_keywords_script.m
+++ get_keywords_script.m
@@ -1,8 +1,17 @@
 [file_name, file_path] = uigetfile( '*.jpg', 'Select the image', '../testing' );
 I = imread( fullfile( file_path, file_name ) );
 
-Q = generateKeywordsGrid( I, fcenter,       cfPwJ_MLE,    cfPwJ_GRE,    cfPbJ_MLE,    cfPbJ_GRE,    0.1, 0.7, 15, number_of_words );
-% Q = generateKeywordsGrid( I, grid_kmeans_C, grid_PwJ_MLE, grid_PwJ_GRE, grid_PbJ_MLE, grid_PbJ_GRE, 0.1, 0.7, 15, number_of_words );
+%Q1 = generateKeywordsGrid( I, fcenter,       cfPwJ_MLE,    cfPwJ_GRE,    cfPbJ_MLE,    cfPbJ_GRE,    0.1, 0.7, 15, number_of_words );
+%Q2 = generateKeywordsGrid( I, grid_kmeans_C, grid_PwJ_MLE, grid_PwJ_GRE, grid_PbJ_MLE, grid_PbJ_GRE, 0.1, 0.7, 15, number_of_words );
+%Q2 = generateKeywordsGrid( I, fcenter,       fPwJ_MLE,    fPwJ_GRE,    fPbJ_MLE,    fPbJ_GRE,    0.1, 0.7, 15, number_of_words );
 
-[tmp2, tmp2_IDX] = sort( Q, 'descend' );
+Q_CFCMRM = generateKeywordsGrid( I, fcenter, cfPwJ_MLE12, cfPwJ_GRE12, cfPbJ_MLE12, cfPbJ_GRE12, 0.1, 0.7, 15, number_of_words );
+Q_FCMRM = generateKeywordsGrid(  I, fcenter,  fPwJ_MLE12,  fPwJ_GRE12,  fPbJ_MLE12,  fPbJ_GRE12, 0.1, 0.7, 15, number_of_words );
+
+fprintf('CFCMRM:');
+[~, tmp2_IDX] = sort( Q_CFCMRM, 'descend' );
+words(tmp2_IDX(1:5))
+
+fprintf('FCMRM:')
+[~, tmp2_IDX] = sort( Q_FCMRM, 'descend' );
 words(tmp2_IDX(1:5))
